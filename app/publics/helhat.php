bdcaa639ef3a57c61c08a27aa20f334baf4f8bea
Skihat-Team <skihat-team@outlook.com>
2015-04-07 21:28:51 +0800

    根据部署更新部分内容。

---
+++ app/publics/helhat.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <title>Hello Skihat</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Hello Skihat</h1>
+
+<div>
+    <?php phpinfo(); ?>
+</div>
+</body>
+</html>
